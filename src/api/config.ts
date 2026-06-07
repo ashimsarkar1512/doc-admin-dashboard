fc24359985898e007b23a6509c3c307a14981b29
AkashMadbor <akashabrar@softvence.com>
2026-06-07 14:47:22 +0600

    this commit for changes the new url to env and config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://pre.weightlossmdcherrycreek.com/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://prod.weightlossmdcherrycreek.com/api/v1';
